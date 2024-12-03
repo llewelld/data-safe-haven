ed0ae2bb7293b507325712b455d080baf47faef6
Matt Craddock <5796417+craddm@users.noreply.github.com>
2024-12-03 09:49:11 +0000

    exclude security_checklist_template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -64,7 +64,7 @@ templates_path = ["_templates"]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["**/*.partial.md"]
+exclude_patterns = ["**/*.partial.md", "**/security_checklist_template.md"]
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
