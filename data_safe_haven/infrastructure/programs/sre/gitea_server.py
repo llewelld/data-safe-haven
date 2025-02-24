a03dccb67d33d272836c60f875e72409dfe37a12
jemrobinson <jemrobinson@users.noreply.github.com>
2025-02-24 03:07:23 +0000

    :arrow_up: Update Pulumi Docker images

--- data_safe_haven/infrastructure/programs/sre/gitea_server.py
+++ data_safe_haven/infrastructure/programs/sre/gitea_server.py
@@ -214,7 +214,7 @@ class SREGiteaServerComponent(ComponentResource):
                     ],
                 ),
                 containerinstance.ContainerArgs(
-                    image="gitea/gitea:1.23.3",
+                    image="gitea/gitea:1.23.4",
                     name="gitea"[:63],
                     command=["/app/custom/entrypoint.sh"],
                     environment_variables=[
