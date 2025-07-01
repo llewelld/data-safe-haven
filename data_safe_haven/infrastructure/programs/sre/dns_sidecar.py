9eecd9e94386c5887f91c800f7364860951dd109
James Robinson <james.em.robinson@gmail.com>
2025-07-01 08:17:55 +0100

    :memo: Fix comment

--- data_safe_haven/infrastructure/programs/sre/dns_sidecar.py
+++ data_safe_haven/infrastructure/programs/sre/dns_sidecar.py
@@ -154,8 +154,7 @@ class DnsSidecarComponent(ComponentResource):
                 opts=child_opts,
             )
 
-            # Allowing the managed identity to retrieve the container group IP
-
+            # Allow the managed identity to retrieve the container group IP
             container_group_role_definition = authorization.RoleDefinition(
                 f"{self._name}_{container_instance.dns_record_name}_dnssidecar_ip_reader_role",
                 role_name=f"Container group reader for {container_instance.dns_record_name} ({stack_name})",
