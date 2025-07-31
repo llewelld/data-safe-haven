b871dce8ecc2233f4d9c685469fda23a29750264
Carlos Gavidia-Calderon <cgavidia-calderon@turing.ac.uk>
2025-07-31 14:24:26 +0100

    Removing allow_workspace_internet from pulumi_vars.yaml

--- data_safe_haven/infrastructure/programs/sre/desired_state.py
+++ data_safe_haven/infrastructure/programs/sre/desired_state.py
@@ -183,7 +183,6 @@ class SREDesiredStateComponent(ComponentResource):
                 ldap_user_filter=props.ldap_user_filter,
                 ldap_user_search_base=props.ldap_user_search_base,
                 software_repository_hostname=props.software_repository_hostname,
-                allow_workspace_internet=props.allow_workspace_internet,
                 use_package_proxies=not props.allow_workspace_internet,
             ).apply(lambda kwargs: StringAsset(self.ansible_vars_file(**kwargs))),
         )
